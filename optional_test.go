66b7f5837bc4278e2ed52835312516a0802e5c93
Austin Ward <ward.austin28@gmail.com>
2019-03-06 19:59:43 -0500

    Adding IfPresent test code

--- optional_test.go
+++ optional_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+const (
+	TEST_STR = "test string"
+)
+
 func shouldPanic(func_name string, t *testing.T) {
 	if r := recover(); r == nil {
 		t.Fatal(func_name + " should panic and did not.")
@@ -17,7 +21,7 @@ func shouldNotPanic(func_name string, t *testing.T) {
 	}
 }
 
-func Test_OptionalOf(t *testing.T) {
+func Test_Of(t *testing.T) {
 	t.Run("Testing Of", Of_test)
 	t.Run("Testing nil Of", OfNil_test)
 }
@@ -25,15 +29,13 @@ func Test_OptionalOf(t *testing.T) {
 func Of_test(t *testing.T) {
 	defer shouldNotPanic("optional.Of with non nil value", t)
 
-	test_val := "test"
-
 	var o *op.Optional
-	o = op.Of(test_val)
+	o = op.Of(TEST_STR)
 
 	if v, ok := o.Get().(string); !ok {
 		t.Error("Returned type should be string.")
-	} else if v != test_val {
-		t.Errorf("Expected `%v`, got `%v`", test_val, v)
+	} else if v != TEST_STR {
+		t.Errorf("Expected `%v`, got `%v`", TEST_STR, v)
 	}
 }
 
@@ -44,22 +46,21 @@ func OfNil_test(t *testing.T) {
 	t.Fatal("this code should not be reachable.")
 }
 
-func Test_OptionalOfNilable(t *testing.T) {
-	t.Run("Testing OfNilable", OfNullable_test)
-	t.Run("Testing nil OfNilable", OfNullableNil_test)
+func Test_OfNilable(t *testing.T) {
+	t.Run("Testing OfNilable", OfNilable_test)
+	t.Run("Testing nil OfNilable", OfNilableNil_test)
 }
 
 func OfNilable_test(t *testing.T) {
 	defer shouldNotPanic("optional.OfNilable", t)
-	test_val := "test"
 
 	var o *op.Optional
-	o = op.OfNilable(test_val)
+	o = op.OfNilable(TEST_STR)
 
 	if v, ok := o.Get().(string); !ok {
 		t.Error("Returned type should be string.")
-	} else if v != test_val {
-		t.Errorf("Expected `%v`, got `%v`", test_val, v)
+	} else if v != TEST_STR {
+		t.Errorf("Expected `%v`, got `%v`", TEST_STR, v)
 	}
 }
 
@@ -72,4 +73,36 @@ func OfNilableNil_test(t *testing.T) {
 	if o.Get() != nil {
 		t.Error("Returned type should be nil but is not.")
 	}
+}
+
+func Test_IfPresent(t *testing.T) {
+	t.Run("Testing IfPresent", IfPresent_test)
+	t.Run("Testing nil IfPresent not present", IfNotPresent_test)
+}
+
+func IfPresent_test(t *testing.T) {
+	defer shouldNotPanic("optional.IfPresent", t)
+
+	var o *op.Optional
+	o = op.Of(TEST_STR)
+
+	ok := false
+	o.IfPresent(func(v op.T) {
+		ok = true
+	})
+
+	if !ok {
+		t.Error("IfPresent was not reached when it should have been.")
+	}
+}
+
+func IfNotPresent_test(t *testing.T) {
+	defer shouldNotPanic("optional.IfPresent", t)
+
+	var o *op.Optional
+	o = op.OfNilable(nil)
+
+	o.IfPresent(func(v op.T) {
+		t.Error("IfPresent was reached when it should not have been.")
+	})
 }
